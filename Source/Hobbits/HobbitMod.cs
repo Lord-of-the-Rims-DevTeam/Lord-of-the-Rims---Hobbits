a60505261f2f1c7cdef51c5199bfa50522a1b3b5
jecrell <matt.walls31@gmail.com>
2018-02-04 00:16:46 +0900

    Initial commit

---
+++ Source/Hobbits/HobbitMod.cs
@@ -0,0 +1,11 @@
+﻿using Verse;
+
+namespace LordOfTheRims_Hobbits
+{
+    public class HobbitMod : Mod
+    {
+        public HobbitMod(ModContentPack content) : base(content)
+        {
+        }
+    }
+}
